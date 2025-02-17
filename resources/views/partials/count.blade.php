c7237081f7454bcff43b37509a3d60b9579521dd
“putraerdin251” <ivadwiriani@gmail.com>
2025-02-17 09:27:52 +0700

    comment content

--- resources/views/partials/count.blade.php
+++ resources/views/partials/count.blade.php
@@ -5,29 +5,29 @@
             <div class="d-flex flex-column align-items-center px-3 py-2 rounded-4 shadow-lg" style="background-color: #5A4DD8; width: 150px; height: 130px">
                 <i class="bi bi-card-list fs-4 p-1 text-white rounded mb-1"></i> 
                 <h6 class="mt-1 mb-0 fs-6">Jumlah Task :</h6>
-                <p class="mb-0 fw-bold fs-5">{{ $lists->count() }}</p>
+                <p class="mb-0 fw-bold fs-5">{{ $lists->count() }}</p> {{--variabel lists count adalah jumlah yang diambil dari variabel di taskcontroller--}}
             </div>
             <div class="d-flex flex-column align-items-center px-3 py-2 rounded-4 shadow-lg" style="background-color: #0CB3E6; width: 150px; height: 130px">
                 <i class="bi bi-star fs-4 p-1 text-white rounded mb-1"></i> 
                 <h6 class="mt-1 mb-0 fs-6">Tugas Low :</h6>
-                <p class="mb-0 fw-bold fs-5">{{ $lowPriorityCount }}</p>
+                <p class="mb-0 fw-bold fs-5">{{ $lowPriorityCount }}</p> {{--variabel ini diambil dari taskcontroller--}}
             </div>
             <div class="d-flex flex-column align-items-center px-3 py-2 rounded-4 shadow-lg" style="background-color: #E05585; width: 150px; height: 130px">
                 <i class="bi bi-star-half fs-4 p-1 text-white rounded mb-1"></i> 
                 <h6 class="mt-1 mb-0 fs-6">Tugas Medium :</h6>
-                <p class="mb-0 fw-bold fs-5">{{ $mediumPriorityCount }}</p>
+                <p class="mb-0 fw-bold fs-5">{{ $mediumPriorityCount }}</p> {{--variabel ini diambil dari taskcontroller--}}
             </div>
             <div class="d-flex flex-column align-items-center px-3 py-2 rounded-4 shadow-lg" style="background-color: #4A6EDD; width: 150px; height: 130px">
                 <i class="bi bi-star-fill fs-4 p-1 text-white rounded mb-1"></i> 
                 <h6 class="mt-1 mb-0 fs-6">Tugas High :</h6>
-                <p class="mb-0 fw-bold fs-5">{{ $highPriorityCount }}</p>
+                <p class="mb-0 fw-bold fs-5">{{ $highPriorityCount }}</p> {{--variabel ini diambil dari taskcontroller--}}
             </div>
             <div class="d-flex flex-column align-items-center px-3 py-2 rounded-4 shadow-lg" style="background-color: #E3651D; width: 145px; height: 130px">
                 <i class="bi bi-card-list fs-4 p-1 text-white rounded mb-1"></i> 
                 <h6 class="mt-1 mb-0 fs-6">Belum selesai :</h6>
-                <p class="mb-0 fw-bold fs-5">{{ $uncompletedCount }}</p>
+                <p class="mb-0 fw-bold fs-5">{{ $uncompletedCount }}</p> {{--variabel ini diambil dari taskcontroller--}}
             </div>
         </div>
     </div>
 </div>
-</div>
+</div> {{-----ini digunakan untuk menampilkan jumlah task diatas content----}}
