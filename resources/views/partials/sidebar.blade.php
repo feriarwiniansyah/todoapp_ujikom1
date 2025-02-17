dab78b7a5fa94580e004303a28c3555f7bbd2266
“putraerdin251” <ivadwiriani@gmail.com>
2025-02-17 13:10:56 +0700

    menambahkan profile

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,17 +1,28 @@
 <div class="d-flex">
     <!-- Sidebar -->
-    <div class="d-flex flex-column flex-shrink-0 p-3 text-white" style="width: 175px; background-color:  #0B2D64; height: 120vh">
-        <div class="row my-3">
-            <div class="col-12">
-                <form action="{{ route('home') }}" method="GET" class="d-flex gap-2">
-                    <input type="text" class="form-control w-100" name="query" placeholder="Cari task/list" 
-                    value="{{ request()->query('query') }}">
-                    <button type="submit" class="btn btn-outline-primary"><i class="bi bi-search"></i></button>
-                </form>
+    <div class="d-flex flex-column flex-shrink-0 p-3 text-white" style="width: 180px; background-color:  #0B2D64; height: 120vh">
+        <div class="d-flex flex-column align-items-center justify-content-center">
+            <div class="mb-2">
+                <img class="rounded-circle" src="{{ asset('image/feri.jpg')}}" alt="Feri" style="width: 60px; height: 60px; object-fit: cover;">
+            </div>
+            <div>
+                <h6 class="text-center">Feri Arwiniansyah</h6>
             </div>
         </div>
+        
         <hr>
         <ul class="nav nav-pills flex-column mb-auto">
+            <li>
+                <div class="row my-3">
+                    <div class="col-12">
+                        <form action="{{ route('home') }}" method="GET" class="d-flex gap-2">
+                            <input type="text" class="form-control w-100" name="query" placeholder="Cari task/list" 
+                            value="{{ request()->query('query') }}">
+                            <button type="submit" class="btn btn-outline-primary"><i class="bi bi-search"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </li>
             <li class="nav-item">
                 <a href="{{route('tasks.index')}}" class="nav-link text-white">
                     <i class="bi bi-house-door me-2"></i> Dashboard
@@ -23,19 +34,5 @@
                 </a>
             </li>
         </ul>
-        
-        <hr>
-        <div class="dropdown">
-            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="bi bi-person-circle fs-4 me-2"></i>
-                <strong>User</strong>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser">
-                <li><a class="dropdown-item" href="#">Profile</a></li>
-                <li><a class="dropdown-item" href="#">Settings</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Sign out</a></li>
-            </ul>
-        </div>
     </div>
 </div>
