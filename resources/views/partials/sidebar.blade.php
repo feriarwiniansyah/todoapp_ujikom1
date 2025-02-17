c4cd580503b04b778243d3d4730c5aaa8eee03b8
“putraerdin251” <ivadwiriani@gmail.com>
2025-02-17 13:21:32 +0700

    menambahkan file about

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,14 +1,16 @@
 <div class="d-flex">
     <!-- Sidebar -->
     <div class="d-flex flex-column flex-shrink-0 p-3 text-white" style="width: 180px; background-color:  #0B2D64; height: 120vh">
-        <div class="d-flex flex-column align-items-center justify-content-center">
-            <div class="mb-2">
-                <img class="rounded-circle" src="{{ asset('image/feri.jpg')}}" alt="Feri" style="width: 60px; height: 60px; object-fit: cover;">
-            </div>
-            <div>
-                <h6 class="text-center">Feri Arwiniansyah</h6>
-            </div>
-        </div>
+        <a href="">
+            <div class="d-flex flex-column align-items-center justify-content-center">
+                <div class="mb-2">
+                    <img class="rounded-circle" src="{{ asset('image/feri.jpg')}}" alt="Feri" style="width: 60px; height: 60px; object-fit: cover;">
+                </div>
+                <div>
+                    <h6 class="text-center">Feri Arwiniansyah</h6>
+                </div>
+            </div>    
+        </a>
         
         <hr>
         <ul class="nav nav-pills flex-column mb-auto">
