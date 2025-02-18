32a0d68a2a54d76fdef29ad506c13c0dcd07206e
“putraerdin251” <ivadwiriani@gmail.com>
2025-02-18 10:39:38 +0700

    comment taskcontroller

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [TaskController::class, 'index'])->name('home'); //digunakan untuk route menampilkan index.blade
 Route::get('/about', [TaskController::class, 'about'])->name('about'); //digunakan sebagai route untuk menampilkan about.blade
 
-Route::resource('lists', TaskListController::class);
+Route::resource('lists', TaskListController::class); 
 
 Route::resource('tasks', TaskController::class);
-Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
+Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete'); //digunakan sebagai route untuk menampilkan complete.blade
 Route::patch('/tasks/{task}/change-list', [TaskController::class, 'changeList'])->name('tasks.changeList');
