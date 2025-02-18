9104512204b90f2cfa29605afda0049f9460cac9
“putraerdin251” <ivadwiriani@gmail.com>
2025-02-18 10:47:22 +0700

    comment tasklistmodel

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -6,15 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaskList extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name']; //untuk menentukan colom mana yang boleh diisi
     
     protected $guarded = [
         'id',
         'created_at',
         'updated_at'
-    ];
+    ]; //untuk menentukan colom mana yang tidak boleh diisi
 
     public function tasks() {
-        return $this->hasMany(Task::class, 'list_id');
+        return $this->hasMany(Task::class, 'list_id'); //digunakan untuk menghubungkan ke table task
     }
 }
