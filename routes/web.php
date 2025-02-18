2c13113457c529200a863df5f53284d330d60cea
“putraerdin251” <ivadwiriani@gmail.com>
2025-02-18 11:53:46 +0700

    comment route/web

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [TaskController::class, 'index'])->name('home'); //digunakan untuk route menampilkan index.blade
 Route::get('/about', [TaskController::class, 'about'])->name('about'); //digunakan sebagai route untuk menampilkan about.blade
 
-Route::resource('lists', TaskListController::class); 
+Route::resource('lists', TaskListController::class); //digunakan sebagai untuk megambil seluruh function di tasklistcontroller
 
-Route::resource('tasks', TaskController::class);
-Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete'); //digunakan sebagai route untuk menampilkan complete.blade
-Route::patch('/tasks/{task}/change-list', [TaskController::class, 'changeList'])->name('tasks.changeList');
+Route::resource('tasks', TaskController::class); //digunakan sebagai untuk megambil seluruh function di taskcontroller
+Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete'); //digunakan sebagai route untuk mengupdate status task dari function complete di taskcontroller
+Route::patch('/tasks/{task}/change-list', [TaskController::class, 'changeList'])->name('tasks.changeList'); //digunakan sebagai route untuk mengupdate list task dari function changeList di taskcontroller
