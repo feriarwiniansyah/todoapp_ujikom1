283561ee6fe3982834f06b9f945d8a7dd0387fc8
“putraerdin251” <ivadwiriani@gmail.com>
2025-02-18 11:45:04 +0700

    comment navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,8 +1,9 @@
-<nav class="navbar navbar-dark rounded-pill w-100" style="background-color: #0B2D64">
+<nav class="navbar navbar-dark rounded-pill w-100" style="background-color: #0B2D64">{{-- untuk memunculkan navbar|class digunakan untuk bootstrap | navbar-expand-lg agar menjadi responsif sesuai ukuran layar --}}
     <div class="container d-flex justify-content-center">
-        <a class="navbar-brand fw-bolder" href="#">{{ config('app.name') }}</a>
+        <a class="navbar-brand fw-bolder" href="#">
+            {{ config('app.name') }} {{-- href itu sebuah syntax untuk melink | config('app.name')  akan mengambil nama aplikasi dari file konfigurasi Laravel (config/app.php)  --}}
+        </a>
     </div>
 </nav>
 
-{{-- untuk memunculkan navbar|class digunakan untuk bootstrap | navbar-expand-lg agar menjadi responsif sesuai ukuran layar --}}
-{{-- href itu sebuah syntax untuk melink | config('app.name')  akan mengambil nama aplikasi dari file konfigurasi Laravel (config/app.php)  --}}
+
